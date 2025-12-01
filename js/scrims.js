f03eab0482008c207bcf676d0e319be8ba15ff32
woozie <gokcea644@gmail.com>
2025-12-01 19:39:01 +0300

    Update scrims.js

--- js/scrims.js
+++ js/scrims.js
@@ -1,1 +1,20 @@
+(function renderScrims() {
+  const el = document.getElementById("scrimList");
+  const scrims = JSON.parse(localStorage.getItem("scrims") || "[]");
 
+  if (!scrims.length) {
+    el.innerHTML = "<p>Henüz etkinlik eklenmemiş.</p>";
+    return;
+  }
+
+  el.innerHTML = "";
+  scrims.forEach(s => {
+    const div = document.createElement("div");
+    div.className = "card";
+    div.innerHTML = `
+      <strong>${s.name}</strong><br>
+      <small>${s.date}</small>
+    `;
+    el.appendChild(div);
+  });
+})();
