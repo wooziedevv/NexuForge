9e39d8da9b70733f3162b08a92f95df17ebf8c3d
woozie <gokcea644@gmail.com>
2025-12-01 19:38:40 +0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,1 +1,20 @@
+(function renderProducts() {
+  const el = document.getElementById("productList");
+  const products = JSON.parse(localStorage.getItem("products") || "[]");
 
+  if (!products.length) {
+    el.innerHTML = "<p>Henüz ürün eklenmemiş.</p>";
+    return;
+  }
+
+  el.innerHTML = "";
+  products.forEach(p => {
+    const div = document.createElement("div");
+    div.className = "card";
+    div.innerHTML = `
+      <strong>${p.name}</strong><br>
+      <span>${p.price} TL</span>
+    `;
+    el.appendChild(div);
+  });
+})();
