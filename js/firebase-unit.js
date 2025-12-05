279da4c0e02166e778622191fd44de0367355a38
woozie <gokcea644@gmail.com>
2025-12-05 14:41:03 +0300

    Update firebase-unit.js

--- js/firebase-unit.js
+++ js/firebase-unit.js
@@ -1,6 +1,3 @@
-// js/firebase-init.js
-
-// 🔹 Firebase config – SENİN ekranından aldığım değerlerle
 const firebaseConfig = {
   apiKey: "AIzaSyAOQWzJJmaynRXCrQ9PFwygoiND1FGpv6E",
   authDomain: "nexuforge.firebaseapp.com",
@@ -10,5 +7,3 @@ const firebaseConfig = {
   appId: "1:10942528260:web:4ab8d2c39e14885729ae51",
   measurementId: "G-ZD5J8M825M"
 };
-
-// initializeApp burada değil, auth-firebase.js içinde çağırıyorum
