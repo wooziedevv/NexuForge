7c6b3b3fa55c08909b37ced3752fc482e2b69009
woozie <gokcea644@gmail.com>
2025-12-05 14:20:08 +0300

    Update firebase-unit.js

--- js/firebase-unit.js
+++ js/firebase-unit.js
@@ -1,1 +1,15 @@
+// js/firebase-init.js
 
+// Firebase projenin ayarları
+const firebaseConfig = {
+  apiKey: "AIzaSyAOQWzJJmaynRXCrQ9PFwygoiND1FGpv6E",
+  authDomain: "nexuforge.firebaseapp.com",
+  projectId: "nexuforge",
+  storageBucket: "nexuforge.firebasestorage.app",
+  messagingSenderId: "10942528260",
+  appId: "1:10942528260:web:4ab8d2c39e14885729ae51",
+  measurementId: "G-ZD5J8M825M"
+};
+
+// initializeApp kısmını auth-firebase.js içinde yapıyoruz,
+// bu dosyada ekstra bir şey yazmana gerek yok.
